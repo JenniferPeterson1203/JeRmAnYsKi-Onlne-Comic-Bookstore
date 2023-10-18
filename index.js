6bb356989cb1dabb90915cbfcb8712441d973792
Jennifer <jenniferpeterson@pursuit.org>
2023-10-18 02:29:03 -0400

    created the showByName, showById, create, update  and showCart functions

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const books = readJSONFile("./data", "bookStoreCart.json");
 const bookInventory = readJSONFile("./data", "bookStoreInventory.json");
 // console.log(bookInventory);
 
+console.log(books);
+
 //import the functions created in the Controller file
-const { addToCart } = require("./src/bookStoreController");
+const {
+  addToCart,
+  create,
+  update,
+  showByName,
+  showById,
+  showCart,
+} = require("./src/bookStoreController");
+
 // create an alias called inform to store the console.log function
 // When providing user feedback in the terminal use `inform`
 // When developing/debugging use `console.log`
@@ -18,48 +28,85 @@ const inform = console.log;
 
 //the run function
 function run() {
-  //process.argv
+  //process.argv (these are all coming from the command line)
   const action = process.argv[2];
   const bookTitle = process.argv[3];
+  const inputAuthor = process.argv[4];
+  const inputReleaseDate = process.argv[5];
+  const inputPriceInCents = process.argv[6];
+  const inputSaleItem = process.argv[7];
+  const inputInStock = process.argv[8];
+  const inputId = process.argv[9];
 
   //write to file toggle
   let writeToFile = false;
   let updatedCart;
 
   switch (action) {
-    case "cart":
-      inform(action, books);
+    case "showCart":
+      const custCart = showCart(books);
+      inform(custCart);
       break;
+
     case "create":
-      inform(action, book);
+      const newInventory = create(
+        bookInventory,
+        bookTitle,
+        inputAuthor,
+        inputReleaseDate,
+        inputPriceInCents,
+        inputSaleItem,
+        inputInStock
+      );
+      writeJSONFile("./data", "bookStoreInventory.json", newInventory);
       break;
-    case "show":
-      inform(action, book);
+
+    case "showByName":
+      const bookInformation = showByName(bookInventory, bookTitle);
+      inform(bookInformation);
       break;
+
+    case "showById":
+      const bookInfo = showById(bookInventory, process.argv[3]);
+      inform(bookInfo);
+      break;
+
     case "update":
-      inform(action, bookTitle);
+      const updatedInventory = update(
+        bookInventory,
+        bookTitle,
+        inputAuthor,
+        inputReleaseDate,
+        inputPriceInCents,
+        inputSaleItem,
+        inputInStock,
+        inputId
+      );
+      writeJSONFile("./data", "bookStoreInventory.json", updatedInventory);
       break;
+
     case "destroy":
       inform(action, book);
       break;
+
     case "clearCart":
       writeToFile = true;
       updatedCart = clearCart(books);
+
     case "addToCart":
       writeToFile = true;
       updatedCart = addToCart(books, bookInventory, bookTitle);
-      //   inform(action);
       break;
     case "total":
       inform(action);
       break;
     default:
-      writeToFile = true;
-      updatedCart = [];
+      inform("There was an error.");
   }
   if (writeToFile) {
     writeJSONFile("./data", "bookStoreCart.json", updatedCart);
   }
 }
 
+// console.log(updatedCart);
 run();
--- src/bookStoreController.js
+++ src/bookStoreController.js
@@ -1,15 +1,69 @@
 //the action functions goes in the Controller file
 
+const { nanoid } = require("nanoid");
+
 //import the readJSONFile function
 const { readJSONFile } = require("./helpers");
 const cart = readJSONFile("./data", "bookStoreCart.json");
+
+const inventory = readJSONFile("./data", "bookStoreInventory.json");
+
+//update Function
+const update = (
+  inventory,
+  inputName,
+  inputAuthor,
+  inputReleaseDate,
+  inputPriceInCents,
+  inputSaleItem,
+  inputInStock,
+  inputId
+) => {
+  const foundBook = inventory.findIndex((book) => book.id === inputId);
+  const updatedBook = {
+    id: inputId,
+    name: inputName,
+    author: inputAuthor,
+    releaseDate: inputReleaseDate,
+    priceInCents: inputPriceInCents,
+    saleItem: inputSaleItem,
+    inStock: inputInStock,
+  };
+  inventory.splice(foundBook, 1, updatedBook);
+  return inventory;
+};
+
+//create Function
+const create = (
+  inventory,
+  inputName,
+  inputAuthor,
+  inputReleaseDate,
+  inputPriceInCents,
+  inputSaleItem,
+  inputInStock
+) => {
+  const newBook = {
+    id: nanoid(4),
+    name: inputName,
+    author: inputAuthor,
+    releaseDate: inputReleaseDate,
+    priceInCents: inputPriceInCents,
+    saleItem: inputSaleItem,
+    inStock: inputInStock,
+  };
+  inventory.push(newBook);
+
+  return inventory;
+};
+
 //addToCart function
 const addToCart = (cart, inventory, input) => {
   for (let item of inventory) {
     if (
-      (item.name.toLowerCase() === input.toLowerCase() ||
-        item.author.toLowerCase() === input.toLowerCase()) &&
-      item.inStock === true
+      item.name.toLowerCase() === input.toLowerCase() ||
+      item.author.toLowerCase() === input.toLowerCase() ||
+      (item.id === input && item.inStock === true)
     ) {
       cart.push(item);
     }
@@ -17,14 +71,35 @@ const addToCart = (cart, inventory, input) => {
   return cart;
 };
 
-//function
-const clearCart = (cart) => {
-  cart.length = 0;
-  return cart;
+//showById Function
+const showById = (inventory, bookId) => {
+  let message;
+  const book = inventory.find((book) => {
+    if (book.id === bookId) {
+      message = `${book.id} ${book.name} ${book.author} ${book.priceInCents}`;
+    }
+  });
+  return message;
 };
-console.log(clearCart);
 
-module.exports = { addToCart };
+//showCart
+const showCart = (cart) => {
+  let cartContents = "You're cart is currenlty empty";
+  for (let ele of cart) {
+    cartContents = `You now have ${ele.name} ${ele.author}`;
+  }
+  return cartContents;
+};
 
-//   console.log(book.inStock);
-//   console.log(book.name === name);
+//showByName function
+const showByName = (inventory, name) => {
+  let message;
+  const book = inventory.find((book) => {
+    if (book.name === name) {
+      message = `${book.id} ${book.name} ${book.author} ${book.priceInCents}`;
+    }
+  });
+  return message;
+};
+
+module.exports = { addToCart, create, update, showByName, showById, showCart };
